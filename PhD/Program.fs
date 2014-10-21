121e0e9185d0cc9570a1dee569fdbc24e04a1a14
maltic <max@maltic.com>
2014-10-21 14:51:19 +0800

    Improved recurrence
    More constraints

--- PhD/Program.fs
+++ PhD/Program.fs
@@ -33,10 +33,12 @@ let parseSurfaces (str:string) =
 
 let stemScore i j k = 1.0
 let internalLoopScore i k j l = 1.0
-let bulgeScore i k = 1.0
-let hairpinLoopScore i k = 1.0
-let internalDangleScore i k = 1.0
-let extenalDangleScore i k = 1.0
+let bulgeScore i j = 1.0
+let hairpinLoopScore i j = 1.0
+let internalDangleScore i j = 1.0
+let extenalDangleScore i kj= 1.0
+let externalStemBonus i j = 1.0
+let interalStemBonus i j = 1.0
 
 type Model = { 
     stem : int -> int -> int -> double;
@@ -46,6 +48,8 @@ type Model = {
     hairpin : int -> int -> double;
     internalDangle : int -> int -> double;
     extenalDangle : int -> int -> double;
+    externalStemBonus : int -> int -> double;
+    interalStemBonus : int -> int -> double;
 }
 
 let scoreExternalLoop model loop =
@@ -54,14 +58,14 @@ let scoreExternalLoop model loop =
             model.stem i j sz + 
                 match children with
                 | [Irreducible(i, j); (Reducible(_) as r); Irreducible(k, l)] -> 
-                    model.internalLoop i (j-i+1) k (l-k+1) + scoreInternalSurface model r
+                    model.internalLoop i j k l + scoreInternalSurface model r
                 | [Irreducible(i, j); (Reducible(_) as r)] -> 
-                    model.lbulge i (j-i+1) + scoreInternalSurface model r
+                    model.lbulge i j + scoreInternalSurface model r
                 | [(Reducible(_) as r); Irreducible(i, j)] -> 
-                    model.rbulge i (j-i+1) + scoreInternalSurface model r
-                | [Irreducible(i, j)] -> model.hairpin i (j-i+1)
+                    model.rbulge i j + scoreInternalSurface model r
+                | [Irreducible(i, j)] -> model.hairpin i j
                 | l -> List.fold (fun s t -> match t with 
-                                                | Irreducible(i, j) -> model.internalDangle i (j-i+1) + s
+                                                | Irreducible(i, j) -> model.internalDangle i j + s
                                                 | r -> scoreInternalSurface model r + s) 0.0 l
         | Irreducible(_) -> failwith "Impossible"
     List.fold (fun s t -> match t with 
@@ -73,36 +77,33 @@ let zuker (rna:string) model =
     let dpV = Array2D.create rna.Length rna.Length System.Double.MinValue
     let dpIM = Array2D.create rna.Length rna.Length System.Double.MinValue
     let dpEM = Array.create rna.Length System.Double.MinValue
-    let rec W i j = // i and j begin a loop externally (i-1 and j+1 are bonded)
+    let IL_MAX_SIZE = 30
+    let rec W i j = // i and j begin a loop (i-1 and j+1 are bonded)
         if i > j then 0.0
         elif dpW.[i,j] > System.Double.MinValue then dpW.[i,j]
         else
-            let sz = j-i+1
-            let mutable max = model.hairpin i sz
-            // bulges
-            for k in 1..sz do
-                let tmp = System.Math.Max(model.lbulge i k + V (i+k) j, 
-                            model.rbulge (j-k+1) k + V i (j-k))
+            let mutable max = model.hairpin i j // hairpin
+            for k in i..(j-1) do // k is the start of the succeeding stem, bulge from i, k-1
+                let tmp = model.lbulge i (k-1) + V k j
+                if tmp > max then max <- tmp
+            for k in j..(-1)..i+1 do // k is the end of the precding stem, bulge from k+1, j
+                let tmp = model.rbulge (k+1) j + V i k
                 if tmp > max then max <- tmp
             // internal loops
-            let maxILSZ = System.Math.Max(sz, 30)
-            for k in 1..maxILSZ do
-                for l in 1..maxILSZ do
-                    if i+k-1 < j-l+1 then
-                        let tmp = model.internalLoop i k j l + V (i+k) (j-l)
-                        if tmp > max then max <- tmp
-            // try multiloop
-            dpW.[i,j] <- System.Math.Max(max, IM i j)
+            for k in i..System.Math.Min(j-3, i+IL_MAX_SIZE) do // left loop i, k
+                for l in j..(-1)..Array.max [|i+3; j-IL_MAX_SIZE; k+3|] do // right loop l, j
+                    let tmp = model.internalLoop i k l j + V (k+1) (l-1)
+                    if tmp > max then max <- tmp
+            dpW.[i,j] <- System.Math.Max(max, IM i j) // try multiloop
             dpW.[i,j]
     and V i j = // i and j close a stem externally
         if i >= j then 0.0
         elif dpV.[i,j] > System.Double.MinValue then dpV.[i,j]
         else
             let mutable max = System.Double.MinValue
-            for k in 1..(j-i-1) do
-                if i+k-1 < j-k+1 then
-                    let tmp = (model.stem i j k) + W (i+k) (j-k)
-                    if tmp > max then max <- tmp
+            for k in 1..(j-i+1)/2 do // k is stem size
+                let tmp = (model.stem i j k) + W (i+k) (j-k)
+                if tmp > max then max <- tmp
             dpV.[i,j] <- max
             max
     and IM i j = // there is an internal multiloop starting at i and ending at j
@@ -110,9 +111,8 @@ let zuker (rna:string) model =
         elif dpIM.[i,j] > System.Double.MinValue then dpIM.[i,j]
         else
             let mutable max = System.Double.MinValue
-            for k in 1..(j-i+1) do
-                let tmp =  System.Math.Max(V (j-k+1) j + IM i (j-k), 
-                            model.internalDangle (j-k+1) k + IM i (j-k))
+            for k in i..j do // k is start of dangle or stem
+                let tmp =  IM i (k-1) + System.Math.Max(V k j + model.interalStemBonus k j, model.internalDangle k j)
                 if tmp > max then max <- tmp
             dpIM.[i,j] <- max
             max
@@ -121,8 +121,8 @@ let zuker (rna:string) model =
         elif dpEM.[j] > System.Double.MinValue then dpEM.[j]
         else
             let mutable max = System.Double.MinValue
-            for k in 0..j do
-                let tmp = EM (k-1) + System.Math.Max(V k j, model.extenalDangle k (j-k+1))
+            for k in 0..j do // k is start of dangle or stem
+                let tmp = EM (k-1) + System.Math.Max(V k j + model.externalStemBonus k j, model.extenalDangle k j)
                 if tmp > max then max <- tmp
             dpEM.[j] <- max
             max
@@ -131,7 +131,7 @@ let zuker (rna:string) model =
 type GeneticAlgorithm<'a> = { 
     breeder : System.Random -> 'a -> 'a -> 'a; 
     mutator: System.Random -> 'a -> 'a;
-    create : System.Random -> 'a;
+    creator : System.Random -> 'a;
     fitness : 'a -> float;
     elitism : float;
     newBlood : float;
@@ -149,7 +149,7 @@ let runGA r ga population =
         Array.init popSize 
             (fun i ->
                 if i < elitism then population.[i]
-                elif i < elitism + newBlood then ga.create r
+                elif i < elitism + newBlood then ga.creator r
                 else
                     if r.NextDouble() > ga.mutate then
                         ga.breeder r population.[i % selected] population.[i % selected]
@@ -165,22 +165,37 @@ let testFitness i = System.Math.Abs(12.32 - i)
 let testCreate (r:System.Random) = r.NextDouble()
 let testGA = {breeder = testBreeder; mutator = testMutate; fitness = testFitness;
     elitism = 0.1; mutate = 0.8; spawn = 0.0; selection = 0.4; newBlood = 0.05;
-    create = testCreate}
-
+    creator = testCreate}
+     
 let evolve ga popSize steps = 
     let r = System.Random()
     Seq.fold (fun (s:'a[]) _ -> printfn "%f" (ga.fitness s.[0]); runGA r ga s) 
-        (Array.init popSize (fun _ -> ga.create r)) 
+        (Array.init popSize (fun _ -> ga.creator r)) 
         (seq {1..steps})
 
 [<EntryPoint>]
 let main argv = 
-    //let m = {
-    //    stem=stemScore; internalDangle = internalDangleScore; extenalDangle = extenalDangleScore; lbulge = bulgeScore;
-    //    internalLoop = internalLoopScore; hairpin=hairpinLoopScore; rbulge = bulgeScore;} 
+    let m = {
+        stem=stemScore; internalDangle = internalDangleScore; extenalDangle = extenalDangleScore; lbulge = bulgeScore;
+        internalLoop = internalLoopScore; hairpin=hairpinLoopScore; rbulge = bulgeScore; externalStemBonus = externalStemBonus; interalStemBonus = interalStemBonus} 
     //System.Console.ReadLine() |> parseSurfaces |> scoreExternalLoop m |> printfn "%A"
-    evolve testGA 100 300 |> printfn "%A"
-    System.Console.Read() |> ignore
+    //evolve testGA 100 300 |> printfn "%A"
     //let n = 10000000
     //printfn "%A" (parseSurfaces (new string [| for i in 1..n do yield if i <= (n/2) then '(' else ')'|]))
+    let sw = System.Diagnostics.Stopwatch()
+    sw.Start()
+    zuker (new string (Array.create 251 ' ')) m |> printfn "%A"
+    sw.Stop()
+    printfn "%d" sw.ElapsedMilliseconds
+    let sw = System.Diagnostics.Stopwatch()
+    sw.Start()
+    zuker (new string (Array.create 251 ' ')) m |> printfn "%A"
+    sw.Stop()
+    printfn "%d" sw.ElapsedMilliseconds
+    let sw = System.Diagnostics.Stopwatch()
+    sw.Start()
+    zuker (new string (Array.create 251 ' ')) m |> printfn "%A"
+    sw.Stop()
+    printfn "%d" sw.ElapsedMilliseconds
+    System.Console.Read() |> ignore
     0 // return an integer exit code
