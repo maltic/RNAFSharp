0697d8077b50059ed65d73bab1eebd6251fc704f
maltic <max@maltic.com>
2015-02-27 17:47:10 +0800

    Revert "Revert "Began Refactor""
    This reverts commit 944660be06b3f256813f6724b0ad9125493c3b8d.

--- PhD/ModelFunctions.fs
+++ PhD/ModelFunctions.fs
@@ -4,8 +4,7 @@ let jacobsonStockmayerEq a b c v =
     a*v + b*(if v <= 0.0 then 0.0 else System.Math.Log(v, System.Math.E)) + c
 
 let jacobsonStockmayer a b c (values:float[]) data = 
-    let sum = Seq.fold (fun s t -> s+values.[t]) 0.0 data
-    jacobsonStockmayerEq a b c sum
+    jacobsonStockmayerEq a b c (Array.sum values)
 
 let rnaRange (rna:RNAPrimary.Base[]) i j = seq {for k in i..j -> int rna.[k]}
 
--- PhD/RNAPrimary.fs
+++ PhD/RNAPrimary.fs
@@ -1,17 +1,9 @@
 ﻿module RNAPrimary
 
-let BASES = 4
-
 type Base = A = 0 | U = 1 | G = 2 | C = 3
 
-/// Check if a and b form a canonical base pair
-let validPair a b = 
-    let arr = [| a; b |] |> Array.sort
-    match arr with
-    | [| Base.A; Base.U |] -> true
-    | [| Base.U; Base.G |] -> true
-    | [| Base.G; Base.C |] -> true
-    | _ -> false
+// TODO This should be made immutable
+let bases = [| Base.A; Base.U; Base.G; Base.C |]
 
 /// Parse a string into an rna sequence, reports errors
 let parse (str:string) =
